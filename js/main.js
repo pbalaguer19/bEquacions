8596283e22362215a0b20c89e4567320352b9924
Pau Balaguer Llaquet <pbalaguer@ciutadella-131-95.eduroamcc.upf.edu>
2020-01-24 14:09:09 +0100

    Some bugs fixed

--- js/main.js
+++ js/main.js
@@ -2,10 +2,12 @@ function getRndInteger(min, max) {
 	return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
 
-m1 = getRndInteger(0, 10);
-n1 = getRndInteger(0, 10);
-m2 = getRndInteger(0, 10);
-n2 = getRndInteger(0, 10);
+m1 = getRndInteger(-10, 10);
+n1 = getRndInteger(-10, 10);
+do{
+	m2 = getRndInteger(-10, 10);
+}while(m2 == m1);
+n2 = getRndInteger(-10, 10);
 
 equacions = [];
 operacions = [];
@@ -44,7 +46,7 @@ function mostraEquacio(){
 	}
 
 	// M2
-	if(m2 > 1){
+	if(m2 > 1 || m2 < -1){
 		eq = eq + m2 + "x";
 	}else{
 		if(m2 == 0){
@@ -78,75 +80,94 @@ function mostraEquacio(){
 	checkIfResult();
 }
 
+function afegirOperacio(op, num){
+	if(num.includes("-")) operacions.push(op + "(" + num + ")");
+	else operacions.push(op + num);
+}
+
 function suma(){
+	if($("#numero").val() == ""){
+		window.alert("Afegeix alguna expressió!");
+		return;
+	}
 	if(!isNaN($("#numero").val())){
 		n1 = n1 + parseFloat($("#numero").val());
 		n2 = n2 + parseFloat($("#numero").val());
-		operacions.push("+" + $("#numero").val());
+		afegirOperacio("+", $("#numero").val());
 	}else if($("#numero").val().toLowerCase().includes("x")){
 		if($("#numero").val().toLowerCase() == "x"){
 			m1 = m1 + 1;
 			m2 = m2 + 1;
-			operacions.push("+" + $("#numero").val());
 		}else{
 			number = $("#numero").val().substring(0,$("#numero").val().length-1);
 			if(!isNaN(number)){
 				m1 = m1 + parseFloat(number);
 				m2 = m2 + parseFloat(number);
-				operacions.push("+" + $("#numero").val());
 			}else{
 				window.alert("Valor invàlid!");
 			}
 		}
+		afegirOperacio("+", $("#numero").val());
 	}else{
 		window.alert("Valor invàlid!");
 	}
 	mostraEquacio();
 }
 function resta(){
+	if($("#numero").val() == ""){
+		window.alert("Afegeix alguna expressió!");
+		return;
+	}
 	if(!isNaN($("#numero").val())){
 		n1 = n1 - parseFloat($("#numero").val());
 		n2 = n2 - parseFloat($("#numero").val());
-		operacions.push("-" + $("#numero").val());
+		afegirOperacio("-", $("#numero").val());
 	}else if($("#numero").val().toLowerCase().includes("x")){
 		if($("#numero").val().toLowerCase() == "x"){
 			m1 = m1 - 1;
 			m2 = m2 - 1;
-			operacions.push("-" + $("#numero").val());
 		}else{
 			number = $("#numero").val().substring(0,$("#numero").val().length-1);
 			if(!isNaN(number)){
 				m1 = m1 - parseFloat(number);
 				m2 = m2 - parseFloat(number);
-				operacions.push("-" + $("#numero").val());
 			}else{
 				window.alert("Valor invàlid!");
 			}
 		}
+		afegirOperacio("-", $("#numero").val());
 	}else{
 		window.alert("Valor invàlid!");
 	}
 	mostraEquacio();
 }
 function divisio(){
+	if($("#numero").val() == ""){
+		window.alert("Afegeix alguna expressió!");
+		return;
+	}
 	if(!isNaN($("#numero").val())){
 		n1 = n1 / parseFloat($("#numero").val());
 		n2 = n2 / parseFloat($("#numero").val());
 		m1 = m1 / parseFloat($("#numero").val());
 		m2 = m2 / parseFloat($("#numero").val());
-		operacions.push("%" + $("#numero").val());
+		afegirOperacio("÷", $("#numero").val());
 	}else{
 		window.alert("Valor invàlid!");
 	}
 	mostraEquacio();
 }
 function multiplicacio(){
+	if($("#numero").val() == ""){
+		window.alert("Afegeix alguna expressió!");
+		return;
+	}
 	if(!isNaN($("#numero").val())){
 		n1 = n1 * parseFloat($("#numero").val());
 		n2 = n2 * parseFloat($("#numero").val());
 		m1 = m1 * parseFloat($("#numero").val());
 		m2 = m2 * parseFloat($("#numero").val());
-		operacions.push("*" + $("#numero").val());
+		afegirOperacio("·", $("#numero").val());
 	}else{
 		window.alert("Valor invàlid!");
 	}
@@ -166,7 +187,7 @@ function checkIfResult(){
 function passos(){
 	llistaPassos = ""
 	for(var i = 0; i<equacions.length; i++){
-		if(i>0) llistaPassos += "[" + operacions[i-1] + "]\n" + equacions[i] + "\n";
+		if(i>0) llistaPassos += "[ Operació: " + operacions[i-1] + "]\n" + equacions[i] + "\n";
 		else llistaPassos += equacions[i] + "\n";
 	}
 	window.alert(llistaPassos);
@@ -176,26 +197,36 @@ function reinicia(){
 	location.reload();
 }
 
-function dibuixa(){
-	tmp = [];
-	// Balança 1
-	for(var i=0; i<m1; i++){
-		tmp.push({ id: i, label: "x", shape: "box", color: "#97C2FC", x:-400, y:35*i })
+function crearNodes(tmp, n, id, label, shape, colorPos, colorNeg, x, y, to){
+	color = colorPos;
+	lab = label;
+	if(n < 0){
+		color = colorNeg;
+		lab = "-" + label;
 	}
-	for(var i=0; i<n1; i++){
-		tmp.push({ id: m1+i, label: "1", shape: "ellipse", color: "#6E6EFD", x:-350, y:35*i })
-	}
-
-	// Balança 2
-	for(var i=0; i<m2; i++){
-		tmp.push({ id: i+n1+m1, label: "x", shape: "box", color: "#97C2FC", x:400, y:35*i})
-	}
-	for(var i=0; i<n2; i++){
-		tmp.push({ id: m2+i+n1+m1, label: "1", shape: "ellipse", color: "#6E6EFD", x:450, y:35*i})
+	for(var i=0; i<Math.abs(parseInt(n)); i++){
+		tmp.push({ id: id+i, label: label, shape: shape, color: color, x:x, y:y*i })
+		if(n < 0){
+			edges.push({ from: id+i, to: to });
+		}
 	}
 
-	var multip = Math.max(n1, m1, n2, m2);
-	console.log(multip);
+	if(parseInt(n) != n){
+		lab = (n - parseInt(n)).toFixed(2);
+		if(label.includes("x")){
+			if (lab < 0) lab = "-" + Math.abs(lab).toString() + "x";
+			else lab = lab.toString() + "x";
+		}
+		tmp.push({ id: 999+id+i, label: lab.toString(), shape: shape, color: color, x:x, y:y*i })
+		if(n < 0){
+			edges.push({ from: id+i, to: to });
+		}
+	}
+
+	return tmp;
+}
+
+function dibuixaBalanca(tmp, multip){
 	// Barra esquerra
 	for(var i=0; i<8; i++){
 		tmp.push({ id: 100 + i, label: "", shape: "box", color: "#000000", x:-450+(i*20), y:(35*multip)+100 })
@@ -213,11 +244,30 @@ function dibuixa(){
 		i++;
 	}
 	tmp.push({ id: 300 + i, label: "", shape: "triangle", color: "#000000", x:0, y:(35*multip)+180 })
+	return tmp;
+}
+
+function dibuixa(){
+	tmp = [];
+	edges = [];
+	// Balança 1
+	tmp = crearNodes(tmp, m1, 0, "x", "box", "#97C2FC", "#DF644E", -400, 35, 104);
+	tmp = crearNodes(tmp, n1, Math.abs(m1), "1", "ellipse", "#6E6EFD", "#902A18", -350, 35, 104);
+
+	// Balança 2
+	tmp = crearNodes(tmp, m2, Math.abs(n1)+Math.abs(m1), "x", "box", "#97C2FC", "#DF644E", 400, 35, 204);
+	tmp = crearNodes(tmp, n2, Math.abs(m2)+Math.abs(n1)+Math.abs(m1), "1", "ellipse", "#6E6EFD", "#902A18", 450, 35, 204);
+
+	var multip = Math.max(Math.abs(n1), Math.abs(m1), Math.abs(n2), Math.abs(m2));
+	tmp = dibuixaBalanca(tmp, multip);
+
 	var nodes1 = new vis.DataSet(tmp);
+	var edges1 = new vis.DataSet(edges);
 
 	var container1 = document.getElementById("mynetwork1");
 	var data1 = {
 		nodes: nodes1,
+		edges: edges1
 	};
 
 	var options = {
